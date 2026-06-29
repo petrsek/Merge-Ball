36a8fa2397f84dae93d195b044a23c9912708a0f
Petr Halbich <pehalbi@zcu.students.czstudents.zsu.cz>
2026-06-29 21:42:26 +0200

    Changed build to release

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782758650|2993006162';
+const CACHE_VERSION = '1782762075|57299790';
 /** @type {string} */
 const CACHE_PREFIX = 'Merge Ball-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
