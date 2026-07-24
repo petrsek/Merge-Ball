fdf7f7068ed2f9465661a8056398d48bd19f552c
Petr Halbich <peta.halbich@gmail.com>
2026-07-24 21:12:41 +0200

    Changed score from int to float

--- scripts/score_label.gd
+++ scripts/score_label.gd
@@ -3,7 +3,7 @@ extends Label
 ## Label with added functionality to count score
 
 @export var score_mult_timer: Timer ## Handles expiration of score multiplier
-var score: int = 0 ## The current score
+var score: float = 0.0 ## The current score
 @export var score_coef: float = 50.0 ## Coefficient by which the level is multiplied to get score gain
 var score_mult: float = 1.0 ## Score multiplier for multiple merges in a row
 @export var max_score_mult: float = 5.0 ## Limit amount for the [member ScoreLabel.score_mult]
@@ -18,7 +18,7 @@ func UpdateText() -> void:
 ## Handles addition of score
 func _on_balls_merged(level: int) -> void:
 	# level + 1 because of level 0
-	score += (level + 1) * score_coef * score_mult 
+	score += float(level + 1) * score_coef * score_mult 
 	score_mult_timer.start()
 	score_mult = clampf(score_mult + 1, 1.0, 5.0)
 	UpdateText()
