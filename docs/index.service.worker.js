ffecc86a497983a280f7e1f9135f9560f1075478
Petr Halbich <peta.halbich@gmail.com>
2026-07-24 23:27:31 +0200

    Exported version v1.0.0

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784892226|4517170020';
+const CACHE_VERSION = '1784928419|29526624011';
 /** @type {string} */
 const CACHE_PREFIX = 'Merge Ball-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
