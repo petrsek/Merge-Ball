86dde03ecd1375467c0268f01f159cb592014836
Petr Halbich <peta.halbich@gmail.com>
2026-07-24 23:45:21 +0200

    Fixed wrong z-index

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784928419|29526624011';
+const CACHE_VERSION = '1784929476|110800570';
 /** @type {string} */
 const CACHE_PREFIX = 'Merge Ball-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
